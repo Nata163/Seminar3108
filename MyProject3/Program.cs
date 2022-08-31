6bc4fd6d186b219ae2adc6bbdbdb81b086bbbc40
Nata <nata.9688@mail.ru>
2022-08-31 17:07:32 +0400

    целие числа между отриц и полож числом

---
+++ MyProject3/Program.cs
@@ -0,0 +1,12 @@
+﻿// все целые числа в промежетке от -n до n
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int startNumber = Math.Abs(number) * -1;
+
+while (startNumber <= number)
+{
+    Console.WriteLine(startNumber);
+    startNumber = startNumber+1;
+}
