d951c19368f0942d2a8cffee62b02cbbae5591c0
Nata <nata.9688@mail.ru>
2022-08-31 16:13:05 +0400

    является ли число Квадратом числа

---
+++ MyProject1/Program.cs
@@ -0,0 +1,18 @@
+﻿//является ли первое число квадратом второго int number = int.Parse(Console.ReadLine())
+
+//вводим числа
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+//проверяем равенство и выводим результат
+if (number2 = number1 * number1)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
