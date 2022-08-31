b96fa1ad0e4024933eb1cc119a9e47a18e159ccf
Nata <nata.9688@mail.ru>
2022-08-31 16:44:17 +0400

    день недели

---
+++ MyProject2/Program.cs
@@ -0,0 +1,15 @@
+﻿// название дня недели по номеру
+
+//ввод данных
+
+Console.Write("Введите число от 1 до 7: ");
+int a = int.Parse(Console.ReadLine());
+
+//определение дня недели
+if (a = 1)
+{
+Console.Write("Сегодня ");
+Console.WriteLine(a);
+}
+
+
