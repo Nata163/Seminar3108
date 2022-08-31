592493cf67f76cdc04e638f20eee327d101a1b96
Nata <nata.9688@mail.ru>
2022-08-31 15:50:57 +0400

    Квадрат числа

---
+++ MyProject/Program.cs
@@ -0,0 +1,14 @@
+﻿// квадрат числа
+
+//определение входного числа
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+//получение квадрата входного числа
+int result = number * number;
+
+//вывод результата
+Console.WriteLine($"Квадрат числа {number} = {result}");
+
+int result_2= Convert.ToInt32(Math.Pow(number, 2));
+Console.WriteLine($"Квадрат числа {number} = {result_2}");
