e7c9d32d04e299ede60739640a9f49fcc6be7c24
Nata <nata.9688@mail.ru>
2022-09-06 21:31:55 +0400

    Add seminar 3.1

---
+++ Seminar3.1/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] nums = new int[10] { 10, 24, 45, 67, 77, 10, 11, 1, 66, 44 };
+
+int max = nums[0] ;
+for (int i = 0; i < nums.Length; i++)
+{
+    if(nums[i]>max)
+    {
+        max = nums[i];
+    }
+}
+Console.Clear();
+Console.WriteLine(max);
