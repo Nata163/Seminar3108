8489259198e45120c39376c93513f79cfc4ae999
Nata <nata.9688@mail.ru>
2022-09-06 12:26:54 +0400

    Add seminar 2

--- Seminar2.3/Program.cs
+++ Seminar2.3/Program.cs
@@ -4,7 +4,6 @@ void DeleteSecDigital()
 {
     int x = new Random().Next(100, 1000);
     int x1 = x / 100;
-    int x2 = x % 100;
     int x3 = x % 10;
     int y = x3 + x1*10;
     Console.WriteLine($"сгенерировано случайное число {x}");
--- Seminar2.4/Program.cs
+++ Seminar2.4/Program.cs
@@ -1,15 +1,17 @@
 ﻿// вывод 2 чисел, определение является ли 2ое кратным первому, Если не кратно, то вывод остатка
 
-void RemainderDiv()
+void RemainderDiv(int number1, int number2)
 {
-    int number1 = new Random().Next(1,10);
-    int number2 = new Random().Next(10,1000);
     int result = number2 % number1;
-    Console.WriteLine($"Сгенерировано первое число {number1}");
-    Console.WriteLine($"Сгенерировано второе число {number2}");
-    if (result > 0) Console.WriteLine($"Остаток равен {result}");
-    else Console.WriteLine("Кратно");
+    if (result == 0) Console.WriteLine("Кратно");
+    else Console.WriteLine($"Остаток равен {result}");
 }
 
-RemainderDiv();
-RemainderDiv();
+Console.Write("Введите первое число ");
+    int number1 = int.Parse(Console.ReadLine());
+    Console.Write("Введите второе число ");
+    int number2 = int.Parse(Console.ReadLine());
+
+RemainderDiv(number1, number2);
+Console.WriteLine();
+
