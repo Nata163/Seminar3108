d7e702733424458277d3a6f4abc5ddeb622ed27d
Nata <nata.9688@mail.ru>
2022-09-06 13:11:44 +0400

    Add seminar 2.5 2.6

---
+++ Seminar2.5/Program.cs
@@ -0,0 +1,14 @@
+﻿// принимает на вход 2 числа, проверяет является ли одно квадратом второго
+
+void Square(int num1, int num2)
+{
+    if (num1 == num2 * num2) Console.WriteLine("Первое число является квадратом второго");
+    else if (num2 == num1 * num1) Console.WriteLine("Второе число является квадратом первого");
+    else Console.WriteLine("нет");
+}
+
+Console.WriteLine("Введите первое число ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = int.Parse(Console.ReadLine());
+Square(num1,num2);
---
+++ Seminar2.6/Program.cs
@@ -0,0 +1,11 @@
+﻿// принимает на вход число и проверяет кратноли оно 7 и 23
+
+void Div(int number1)
+{
+    if (number1 % 7+ number1 % 23 == 0) Console.WriteLine("да");
+    else Console.WriteLine("нет");
+}
+
+Console.WriteLine("Введите число ");
+int number1 = int.Parse(Console.ReadLine());
+Div(number1);
