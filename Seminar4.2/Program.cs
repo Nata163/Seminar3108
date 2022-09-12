ad01f2338e453251417def0e8edb1810469f6afe
Nata <nata.9688@mail.ru>
2022-09-12 16:58:22 +0400

    add Seminar4.2

---
+++ Seminar4.2/Program.cs
@@ -0,0 +1,29 @@
+﻿// программа, которая принимает на вход число и выдает количество цифр в числе
+
+int GetCountDigit(int number)
+{
+    //количество цифр в числе
+    int countDigit = 0;
+
+    //временная переменная для хранения входного значения
+    int tempNumber =  Math.Abs(number);
+
+    //Подсчет цифр в входном числе
+    while(tempNumber!=0)
+    {
+        //избавление от правой крайней цифры
+        tempNumber = tempNumber/10;
+
+        // увеличение счетчика на 1
+        countDigit++;
+    }
+    //возврат результата
+return countDigit;
+}
+
+Console.WriteLine("Введите число: ");
+int x = int.Parse(Console.ReadLine());
+
+int countDigit = GetCountDigit(x);
+Console.WriteLine($"Количество цифр в числе {x}: {countDigit}");
+
