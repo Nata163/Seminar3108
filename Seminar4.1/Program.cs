c42851c4e452734273465a21552e2453d2adf2cd
Nata <nata.9688@mail.ru>
2022-09-12 16:22:21 +0400

    add Seminar4.1

--- Seminar4.1/Program.cs
+++ Seminar4.1/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// программа, которая принимает на вход число A и выдает на выход сумму от 1 до А
+
+int GetSum(int number)
+{
+    int sum = 0;
+    if (number > 0)
+    {
+        for (int i = 1; i <= number; i++)
+        {
+            sum = sum + i;
+        }
+    }
+    if (number < 0)
+    {
+        for (int i = 1; i >= number; i--)
+        {
+            sum = sum + i;
+        }
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число: ");
+
+try
+{  
+  int a = int.Parse(Console.ReadLine());
+
+  int sum = GetSum(a);
+  Console.WriteLine(sum);
+}
+catch(Exception ex)
+{
+  Console.WriteLine($"Входное значение имеет неверный формат. Ошибка: {ex.Message}");
+}
