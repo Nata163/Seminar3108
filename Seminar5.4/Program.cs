b49f0ab379139aa627437bc0f52d9a3ef0df0dcd
Nata <nata.9688@mail.ru>
2022-09-15 12:38:24 +0400

    add Seminar5.4

---
+++ Seminar5.4/Program.cs
@@ -0,0 +1,27 @@
+﻿// задайте массив. Напишите программу, котора определяет присутствует ли  данное число в массиве.
+
+bool CheckNumber(int number, int[] array)
+{
+    bool result = false;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == number)
+        {
+            result = true;
+            break;
+        }
+        else result = false;
+    }
+    return result;
+}
+
+int[] arr = new int[] { 6, 7, 19, 345, 3 };
+
+Console.WriteLine("Введите число. которое нужно проверить: ");
+int num = int.Parse(Console.ReadLine());
+
+bool result= CheckNumber(num, arr);
+Console.WriteLine(result);
+
+
+
