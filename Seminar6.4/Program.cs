13212102ded0f64cc8083cd6ed9739690489f207
Nata <nata.9688@mail.ru>
2022-09-19 18:48:19 +0400

    add seminar6.4

---
+++ Seminar6.4/Program.cs
@@ -0,0 +1,21 @@
+﻿// программа, кот преобразовывает числа из деситичных в двоичное
+
+string GetBinary(string num)
+{
+    string res = String.Empty;
+    int i = int.Parse(num);
+    while (i > 0)
+    {
+        int temp = i % 2;
+        res = temp + res;
+        i = i / 2;
+    }
+    return res;
+}
+
+Console.WriteLine("Введите число:");
+string number = Console.ReadLine();
+
+string result = GetBinary(number);
+Console.WriteLine(result);
+
