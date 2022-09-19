5fd17b12ee20b0bbedfd72458bda182441fe335b
Nata <nata.9688@mail.ru>
2022-09-19 14:48:04 +0400

    add seminar6.2

---
+++ Seminar6.2/Program.cs
@@ -0,0 +1,24 @@
+﻿// // переворот одномерного массива без использования второго массива
+int[] GetReverseArray2(int[] array)
+{
+    int tempStorage = array[0];
+    for(int i=0; i<array.Length/2; i++)
+    {
+       tempStorage = array[i];
+       array[i]=array[array.Length-1-i];
+       array[array.Length-1-i]=tempStorage;
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine(arr[i]);
+    }
+}
+
+int[] array= new int[]{1,2,3,4,5};
+int[] result= GetReverseArray2(array);
+PrintArray(result);
