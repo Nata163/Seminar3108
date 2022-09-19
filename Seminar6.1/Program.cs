5b10905e9b80587aae74b110ea1d09319e857527
Nata <nata.9688@mail.ru>
2022-09-19 14:39:07 +0400

    add seminar6.1

---
+++ Seminar6.1/Program.cs
@@ -0,0 +1,23 @@
+﻿// переворот одномерного массива
+
+int[] GetReverseArray(int[] array)
+{
+    int[] result = new int[array.Length];
+    for(int i=0; i<result.Length; i++)
+    {
+       result[i]=array[array.Length-1-i];
+    }
+    return result;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine(arr[i]);
+    }
+}
+
+int[] array= new int[]{1,2,3,4,5};
+int[] result= GetReverseArray(array);
+PrintArray(result);
