fc22cb4bba8c4ccf0ceaf6498585fb0c52538437
Nata <nata.9688@mail.ru>
2022-09-19 18:02:58 +0400

    add seminar6.3

---
+++ Seminar6.3/Program.cs
@@ -0,0 +1,30 @@
+﻿// программа, кот на вход принимает 3 числа и проверяет, может ли существовать треугольник со сторонами такой длины
+
+bool Triagle (int num1, int num2, int num3)
+{
+    bool result =false;
+    if (num1> num2+num3)
+    {
+        result=true;
+    }
+    else if (num2>num1+num3)
+    {
+        result=true;
+    }
+    else if (num3>num1+num2)
+    {
+        result=true;
+    }
+    else result=false;
+    return result;
+}
+
+Console.WriteLine("Введите первое число:");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int number2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число:");
+int number3 = int.Parse(Console.ReadLine());
+
+bool result = Triagle(number1,number2,number3);
+Console.Write(result);
