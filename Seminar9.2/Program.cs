a635dcf11401d81d36f2b318e00b3e0fe5a77240
Nata <nata.9688@mail.ru>
2022-09-28 15:49:34 +0400

    add seminar9.2

---
+++ Seminar9.2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до Т.
+
+string GetAllDigits(int n)
+{
+    if (n == 1)
+    {
+        return "1";
+    }
+    string result = GetAllDigits(n - 1) + " " + n;
+    return result;
+}
+
+int GetSumNumbers(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    int sum = n + GetSumNumbers(n - 1);
+    return sum;
+}
+
+Console.WriteLine("Введите число:");
+int n=int.Parse(Console.ReadLine());
+
+string res= GetAllDigits(n);
+Console.WriteLine($"{res}");
+
+int sum=GetSumNumbers(n);
+Console.WriteLine($"{sum}");
