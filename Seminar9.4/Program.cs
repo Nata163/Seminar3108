0fc52011b211c7d58e055b9d35839a05f351303d
Nata <nata.9688@mail.ru>
2022-09-28 16:22:29 +0400

    add seminar9.4

---
+++ Seminar9.4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа принимает число и выводит сумму цифр в числе
+
+int GetSumDitgit(int n)
+{
+    if (n < 10)
+    {
+        return n;
+    }
+
+    int sum = n % 10 + GetSumDitgit(n / 10);
+    return sum;
+}
+Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine());
+int res = GetSumDitgit(num);
+Console.WriteLine(res);
