69b29d89b9b3597829f160f03f28eb55c7e11fbd
Nata <nata.9688@mail.ru>
2022-09-28 15:09:04 +0400

    add seminar9.1

---
+++ Seminar9.1/Program.cs
@@ -0,0 +1,20 @@
+﻿// найти фактриал при помощи рекурсии
+
+// 5!=1*2*3*4*5=120
+
+int Factorial(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    if (n == 1)
+    {
+        return 1;
+    }
+    int result = n * Factorial(n - 1);
+    return result;
+}
+
+var res=Factorial(5);
+Console.ReadKey();
