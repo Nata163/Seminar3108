e578632419206b3cd86101dc02401268fb9f82d5
Nata <nata.9688@mail.ru>
2022-09-28 16:22:43 +0400

    add seminar9.5

---
+++ Seminar9.5/Program.cs
@@ -0,0 +1,18 @@
+﻿// программа, кот принимает на вход A и B. И возводит a в степень b.
+int GetPower(int a, int b)
+{
+    if (b == 1)
+    {
+        return a;
+    }
+
+    int power = a * GetPower(a , b-1);
+    return power;
+}
+Console.WriteLine("Введите число A:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B:");
+int b = int.Parse(Console.ReadLine());
+
+int res = GetPower(a,b);
+Console.WriteLine(res);
